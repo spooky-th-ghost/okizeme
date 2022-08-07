f0e30263854567b9368915543b05e53d6bf56026
spooky-th-ghost <Colin@wildheartinteractive.com>
2022-08-06 22:03:26 -0400

    created generic timer event that can be used to add all timer types

--- crates/okizeme_systems/src/timers.rs
+++ crates/okizeme_systems/src/timers.rs
@@ -1,5 +1,7 @@
 use bevy::prelude::*;
-use okizeme_types::{Busy, Hitstop, SelfRemoving, Stun};
+use okizeme_types::{
+    AirdashLockout, Busy, Hitstop, JumpLockout, PlayerId, Stun, Timer, TimerEvent, TimerType,
+};
 
 pub fn manage_hitstop(mut coms: Commands, mut query: Query<(Entity, &mut Hitstop)>) {
     for (entity, mut hitstop) in query.iter_mut() {
@@ -21,6 +23,60 @@ pub fn manage_busy(mut coms: Commands, mut query: Query<(Entity, &mut Busy), Wit
     for (entity, mut busy) in query.iter_mut() {
         if busy.is_finished() {
             coms.entity(entity).remove::<Busy>();
+        }
+    }
+}
+
+pub fn manage_airdash_lockout(
+    mut coms: Commands,
+    mut query: Query<(Entity, &mut AirdashLockout), Without<Hitstop>>,
+) {
+    for (entity, mut airdash_lockout) in query.iter_mut() {
+        if airdash_lockout.is_finished() {
+            coms.entity(entity).remove::<AirdashLockout>();
+        }
+    }
+}
+
+pub fn manage_jump_lockout(
+    mut coms: Commands,
+    mut query: Query<(Entity, &mut JumpLockout), Without<Hitstop>>,
+) {
+    for (entity, mut jump_lockout) in query.iter_mut() {
+        if jump_lockout.is_finished() {
+            coms.entity(entity).remove::<JumpLockout>();
+        }
+    }
+}
+
+pub fn add_timers(
+    mut commands: Commands,
+    query: Query<(Entity, &PlayerId)>,
+    mut timer_reader: EventReader<TimerEvent>,
+) {
+    for event in timer_reader.iter() {
+        for (entity, player_id) in query.iter() {
+            if event.player_id == *player_id {
+                match event.timer_type {
+                    TimerType::Hitstop => {
+                        commands.entity(entity).insert(Hitstop(event.duration));
+                    }
+                    TimerType::Stun => {
+                        commands.entity(entity).insert(Stun(event.duration));
+                    }
+                    TimerType::Busy => {
+                        commands.entity(entity).insert(Busy(event.duration));
+                    }
+                    TimerType::JumpLockout => {
+                        commands.entity(entity).insert(JumpLockout(event.duration));
+                    }
+                    TimerType::AirdashLockout => {
+                        commands
+                            .entity(entity)
+                            .insert(AirdashLockout(event.duration));
+                    }
+                }
+            }
         }
     }
 }
--- crates/okizeme_types/src/lib.rs
+++ crates/okizeme_types/src/lib.rs
@@ -12,7 +12,7 @@ pub enum PlayerId {
     P2,
 }
 
-pub trait SelfRemoving {
+pub trait Timer {
     fn countdown(&mut self);
     fn duration(&self) -> u8;
 
@@ -26,9 +26,9 @@ pub trait SelfRemoving {
     }
 }
 
-macro_rules! SelfRemoving {
+macro_rules! Timer {
   (for $($t:ty),+) => {
-      $(impl SelfRemoving for $t {
+      $(impl Timer for $t {
         fn duration(&self) -> u8 {
           self.0
         }
@@ -40,21 +40,41 @@ macro_rules! SelfRemoving {
   }
 }
 
+pub enum TimerType {
+    Hitstop,
+    Stun,
+    Busy,
+    JumpLockout,
+    AirdashLockout,
+}
+pub struct TimerEvent {
+    pub player_id: PlayerId,
+    pub duration: u8,
+    pub timer_type: TimerType,
+}
+
 /// Primarily attached to enties when they should be skipped for animation
 /// and physics calculations
 #[derive(Component, Inspectable)]
 pub struct Hitstop(pub u8);
 
-//Component used to pause input reading and state updates while in block or hit stun
+/// Component used to pause input reading and state updates while in block or hit stun
 #[derive(Component, Inspectable)]
 pub struct Stun(pub u8);
 
-/// Primarily attached to enties when they should be skipped for animation
-/// and physics calculations
+/// Component used to pause input reading while a player is in a busy state
 #[derive(Component, Inspectable)]
 pub struct Busy(pub u8);
 
-SelfRemoving!(for Hitstop, Stun, Busy);
+/// Component used to indicate when a player is unable to jump
+#[derive(Component, Inspectable)]
+pub struct JumpLockout(pub u8);
+
+/// Component used to indicate when a player is unable to airdash
+#[derive(Component, Inspectable)]
+pub struct AirdashLockout(pub u8);
+
+Timer!(for Hitstop, Stun, Busy, JumpLockout, AirdashLockout);
 
 #[derive(Debug, Clone, Eq, PartialEq, Hash)]
 pub enum GameState {
